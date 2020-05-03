70c6b06ba251458d72582f3f94c8a5914515877c
alex <alexvercher@gmail.com>
2020-05-03 12:53:17 +0200

    Primer bloque Spring MVC

--- spring-boot-web/src/main/java/com/avc/springboot/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/avc/springboot/web/app/controllers/IndexController.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.avc.springboot.web.app.models.Usuario;
@@ -16,6 +18,12 @@ import com.avc.springboot.web.app.models.Usuario;
 @RequestMapping("/app")
 public class IndexController {
 
+	@Value("${indexcontroller.index.titulo}")
+	private String textoIndex;
+	@Value("${indexcontroller.perfil.titulo}")
+	private String textoPerfil;
+	@Value("${indexcontroller.listar.titulo}")
+	private String textoListar;
 	/*
 	@GetMapping({"/index", "/", "/home"})
 	public ModelAndView index(ModelAndView mav) {
@@ -26,7 +34,7 @@ public class IndexController {
 	
 	@GetMapping({"/index", "/", "", "/home"})
 	public String index(Model model) {
-		model.addAttribute("titulo", "Hola Spring Framework");
+		model.addAttribute("titulo", textoIndex);
 		return "index";
 	}
 	
@@ -34,19 +42,23 @@ public class IndexController {
 	public String perfil(Model model) {
 		Usuario usuario = new Usuario("Alex", "Vercher", "test@test.com");
 		model.addAttribute("usuario", usuario);
-		model.addAttribute("titulo", "Perfil usuario ".concat(usuario.getNombre()));
+		model.addAttribute("titulo", textoPerfil.concat(usuario.getNombre()));
 		return "perfil";
 	}
 	
 	@RequestMapping("/listar")
 	public String listar(Model model) {
+		model.addAttribute("titulo", textoListar);
+		//model.addAttribute("usuarios", usuarios);
+		return "listar";
+	}
+
+	@ModelAttribute("usuarios")	
+	private List<Usuario> rellenarUsuarios() {
 		List<Usuario> usuarios = Arrays.asList(new Usuario("Alex", "Vercher", "alex@email.com"),
 				new Usuario("Juan", "González", "juan@email.com"),
 				new Usuario("Javier", "Menudillo", "javier@email.com"),
 				new Usuario("Jose", "Pérez", "jose@email.com"));
-		
-		model.addAttribute("titulo", "Listado de usuarios");
-		model.addAttribute("usuarios", usuarios);
-		return "listar";
+		return usuarios;
 	}
 }
